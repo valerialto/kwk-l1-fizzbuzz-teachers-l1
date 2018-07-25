50fe0c01a2c7f43496ada68c08700d179b9fbbb5
valerialto <valeriato14@gmail.com>
2018-07-25 19:01:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,3 +11,13 @@
 # end
 
 # # puts fizzBuzz(15)
+
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
+end
