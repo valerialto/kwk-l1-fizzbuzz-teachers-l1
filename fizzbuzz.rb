324a6e58dcad93a06dbfd8265e775d79b3127055
valerialto <valeriato14@gmail.com>
2018-07-25 18:37:34 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzBuzz(int)
+  if int % 15 == 0
+    return "FizzBuzz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 3 == 0
+    return "Fizz"
+  else
+    return "nil"
+  end
+end
+
+# puts fizzBuzz(15)
